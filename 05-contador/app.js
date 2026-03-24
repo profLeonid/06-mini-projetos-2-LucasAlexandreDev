1123866c700286ed8e004133f9310c469a3d872e
LUCAS ALEXANDRE DA SILVA <25203702@CFP127ALUNO.LOCAL>
2026-03-24 16:24:05 -0300

    feat: adiciona função de handleClick, onde monta a estrutura inteira dentro do tbody

--- 05-contador/app.js
+++ 05-contador/app.js
@@ -84,3 +84,22 @@ const criarLinha = function(numero, par, impar, multiplicativo, potencia){
     
     tbory.appendChild(tr)
 }
+
+
+// função que é chamada pelo onclick do butão, montando a estrutra do tbory com base nas outras funções
+const handleClick = function(){
+
+    const quantidade = Number(document.getElementById('quantidade').value)
+
+    const listaNumeros                = criarListaNumeros        (quantidade) 
+    const listaNumerosPares           = criarListaPares          (quantidade) 
+    const listaNumerosImpares         = criarListaImpares        (quantidade) 
+    const listaNumerosMultiplicativo5 = criarListaMultiplicativa5(quantidade) 
+    const listaNumerosPotencia2       = criarListaPotencia2      (quantidade) 
+
+    document.getElementById('tbody').replaceChildren()
+
+    for(let i = 0; i < quantidade; i++){
+        criarLinha(listaNumeros[i], listaNumerosPares[i], listaNumerosImpares[i], listaNumerosMultiplicativo5[i], listaNumerosPotencia2[i])
+    }
+}
