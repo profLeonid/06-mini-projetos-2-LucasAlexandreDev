7f1557717dff2357a09c1ae3ab26b869cd9326d1
LUCAS ALEXANDRE DA SILVA <25203702@CFP127ALUNO.LOCAL>
2026-03-24 17:04:27 -0300

    feat: adiciona função de Lista de Subtração (exercício 06 - Tabuada)

--- 06-tabuada/app.js
+++ 06-tabuada/app.js
@@ -25,3 +25,13 @@ const criarListaAdicao = function(quantidade){
     return listaAdicao
 }
 
+// função que retornar uma lista de Subtração (de acordo com a quantidade do número que o usuário digitar)
+const criarListaSubtracao = function(quantidade){
+
+    let listaSubtracao = []
+
+    for(let i = 1; i <= quantidade; i++){
+        listaSubtracao.push(i- 1)
+    }
+    return listaSubtracao
+}
